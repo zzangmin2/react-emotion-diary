2f6e3d8678382f679aaa29b28a336bebfad87e31
jungmin <103804129+zzangmin2@users.noreply.github.com>
2023-03-06 21:09:01 +0900

    Add files via upload

--- components/DiaryEditor.js
+++ components/DiaryEditor.js
@@ -1,52 +1,24 @@
 import { useState, useRef, useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { DiaryDispatchContext } from "./../App";
 
 import MyHeader from "./MyHeader";
 import MyButton from "./MyButton";
 import EmotionItem from "./EmotionItem";
-import { DiaryDispatchContext } from "./../App";
+
+import { getStringDate } from "../utill/date";
+import { emotionList } from "../utill/emotion";
 
 const env = process.env;
 env.PUBLIC_URL = env.PUBLIC_URL || "";
-
-const emotionList = [
-  {
-    emotion_id: 1,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion1.png`,
-    emotion_descript: "완전 좋음",
-  },
-  {
-    emotion_id: 2,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion2.png`,
-    emotion_descript: "좋음",
-  },
-  {
-    emotion_id: 3,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion3.png`,
-    emotion_descript: "그럭저럭",
-  },
-  {
-    emotion_id: 4,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion4.png`,
-    emotion_descript: "나쁨",
-  },
-  {
-    emotion_id: 5,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion5.png`,
-    emotion_descript: "끔찍함",
-  },
-];
-
-const getStringDate = (date) => {
-  return date.toISOString().slice(0, 10);
-};
 
 const DiaryEditor = ({ isEdit, originData }) => {
   const contentRef = useRef();
   const [content, setContent] = useState("");
   const [emotion, setEmotion] = useState(3);
   const [date, setDate] = useState(getStringDate(new Date()));
-  const { onCreate } = useContext(DiaryDispatchContext);
+
+  const { onCreate, onEdit, onRemove } = useContext(DiaryDispatchContext);
 
   const handleClickEmote = (emotion) => {
     setEmotion(emotion);
@@ -59,20 +31,51 @@ const DiaryEditor = ({ isEdit, originData }) => {
       return;
     }
 
-    onCreate(date, content, emotion);
+    if (
+      window.confirm(
+        isEdit ? "일기를 수정하시겠습니까?" : "새로운 일기를 작성하시겠습니까?"
+      )
+    ) {
+      if (!isEdit) {
+        onCreate(date, content, emotion);
+      } else {
+        onEdit(originData.id, date, content, emotion);
+      }
+    }
+
     navigate("/", { replace: true });
   };
 
-  // useEffect(() => {
-  //   setDate(getStringDate(new Date(parseInt(originData.date)));
-  // }, [isEdit, originData]);
+  const handleRemove = () => {
+    if (window.confirm("정말 삭제하시겠습니까 ?")) {
+      onRemove(originData.id);
+      navigate("/", { replace: true });
+    }
+  };
+
+  useEffect(() => {
+    if (isEdit) {
+      setDate(getStringDate(new Date(parseInt(originData.date))));
+      setEmotion(originData.emotion);
+      setContent(originData.content);
+    }
+  }, [isEdit, originData]);
 
   return (
     <div className="DiaryEditor">
       <MyHeader
-        headText={"새 일기쓰기"}
+        headText={isEdit ? "일기 수정하기" : "새 일기쓰기"}
         leftChild={
           <MyButton text={"< 뒤로가기"} onClick={() => navigate(-1)} />
+        }
+        rightChild={
+          isEdit && (
+            <MyButton
+              text={"삭제하기"}
+              type={"negative"}
+              onClick={handleRemove}
+            />
+          )
         }
       />
       <div>
@@ -127,3 +130,5 @@ const DiaryEditor = ({ isEdit, originData }) => {
 };
 
 export default DiaryEditor;
+
+//3:36:40
--- components/DiaryItem.js
+++ components/DiaryItem.js
@@ -1,36 +1,40 @@
-import { useNavigate } from 'react-router-dom';
-import MyButton from './MyButton';
+import { useNavigate } from "react-router-dom";
+import MyButton from "./MyButton";
 
+const DiaryItem = ({ id, emotion, content, date }) => {
+  const navigate = useNavigate();
 
-const DiaryItem = ({id, emotion, content, date}) => {
+  const env = process.env;
+  env.PUBLIC_URL = env.PUBLIC_URL || "";
 
-    const navigate = useNavigate();
+  const strDate = new Date(parseInt(date)).toLocaleDateString();
 
-    const env = process.env;
-    env.PUBLIC_URL = env.PUBLIC_URL || "";
+  const goDetail = () => {
+    navigate(`/diary/${id}`);
+  };
 
-    const strDate = new Date(parseInt(date)).toLocaleDateString();
-
-    const goDetail = () => {
-        navigate(` /diary/${id}`)
-    }
-
-    const goEdit = () => {
-        navigate(` /edit/${id}`)
-    }
-    return <div className ="DiaryItem">
-        <div className={["emotion_img_wrapper",`emotion_img_wrapper_${emotion}`].join(" ")}>
-            <img src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`} />
-        </div>
-        <div onClick = {goDetail} className="info_wrapper">
-            <div className = "diary_date">{strDate}</div>
-            <div className = "diary_content_preview">{content.slice(0,25)}</div>
-        </div>
-        <div className ="btn_wrapper">
-            <MyButton onClick={goEdit} text={"수정하기"} />
-        </div>
-
+  const goEdit = () => {
+    navigate(`/edit/${id}`);
+  };
+  return (
+    <div className="DiaryItem">
+      <div
+        className={[
+          "emotion_img_wrapper",
+          `emotion_img_wrapper_${emotion}`,
+        ].join(" ")}
+      >
+        <img src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`} />
+      </div>
+      <div onClick={goDetail} className="info_wrapper">
+        <div className="diary_date">{strDate}</div>
+        <div className="diary_content_preview">{content.slice(0, 25)}</div>
+      </div>
+      <div className="btn_wrapper">
+        <MyButton onClick={goEdit} text={"수정하기"} />
+      </div>
     </div>
-}
+  );
+};
 
 export default DiaryItem;
