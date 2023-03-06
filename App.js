af8ba46181b0ad71e188b04da2b3bdcf0ad6f487
jungmin <103804129+zzangmin2@users.noreply.github.com>
2023-03-06 21:08:34 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -1,111 +1,92 @@
-import React, {useReducer, useRef} from "react";
+import React, { useReducer, useRef, useEffect } from "react";
 
-import './App.css';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import RouteTest from './components/RouteTest';
-import Home from './pages/Home';
-import New from './pages/New';
-import Edit from './pages/Edit';
-import Diary from './pages/Diary';
+import RouteTest from "./components/RouteTest";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Edit from "./pages/Edit";
+import Diary from "./pages/Diary";
 
-const reducer = (state,action) =>{
+const reducer = (state, action) => {
   let newState = [];
 
-  switch(action.type){
-
-    case 'INIT' : {
+  switch (action.type) {
+    case "INIT": {
       return action.data;
     }
-    case 'CREATE' : {
-      newState =[action.data ,...state];
-      break; 
+    case "CREATE": {
+      newState = [action.data, ...state];
+      break;
     }
-    case 'REMOVE' : {
+    case "REMOVE": {
       newState = state.filter((it) => it.id !== action.targetId);
       break;
     }
-    case 'EDIT' : {
-      newState = state.map((it) => it.id === action.data.id? {...action.data}: it);
+    case "EDIT": {
+      newState = state.map((it) =>
+        it.id === action.data.id ? { ...action.data } : it
+      );
       break;
     }
     default:
       return state;
   }
 
+  localStorage.setItem("diary", JSON.stringify(newState));
   return newState;
 };
 
-export const DiaryStateContext = React.createContext(); 
+export const DiaryStateContext = React.createContext();
 export const DiaryDispatchContext = React.createContext();
 
-const dummyData = [
-  {
-    id: 1,
-    emotion: 1,
-    content: "오늘의 일기 1번",
-    date: 1677676808945,
-  },
-  {
-    id: 2,
-    emotion: 2,
-    content: "오늘의 일기 2번",
-    date: 1677676808944,
-  },
-  {
-    id: 3,
-    emotion: 3,
-    content: "오늘의 일기 3번",
-    date: 1677676808943,
-  },
-  {
-    id: 4,
-    emotion: 4,
-    content: "오늘의 일기 4번",
-    date: 1677676808942,
-  },
-  {
-    id: 5,
-    emotion: 5,
-    content: "오늘의 일기 5번",
-    date: 1677676808941,
-  },
-  {
-    id: 6,
-    emotion: 2,
-    content: "오늘의 일기 5번",
-    date: 1777478708596,
-  },
-]
 function App() {
+  const [data, dispatch] = useReducer(reducer, []);
 
-  const [data, dispatch] = useReducer(reducer, dummyData);
+  useEffect(() => {
+    const localData = localStorage.getItem("diary");
+    if (localData) {
+      const diaryList = JSON.parse(localData).sort(
+        (a, b) => parseInt(b.id) - parseInt(a.id)
+      );
+      dataId.current = parseInt(diaryList[0].id) + 1;
+
+      dispatch({ type: "INIT", data: diaryList });
+    }
+  }, []);
 
   const dataId = useRef(0);
 
   //CREATE
-  const onCreate = (date,content, emotion) =>{
-    dispatch({type: "CREATE" ,data:{
-      id: dataId.current,
-      date: new Date(date).getTime(),content,emotion
-    }})
+  const onCreate = (date, content, emotion) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: dataId.current,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
     dataId.current += 1;
-  }
+  };
   //REMOVE
-  const onRemove = (targetId) =>{
-    dispatch({type: "REMOVE" , targetId});
-  }
+  const onRemove = (targetId) => {
+    dispatch({ type: "REMOVE", targetId });
+  };
   //EDIT
-  const onEdit = (targetId,date,content,emotion) =>{
+  const onEdit = (targetId, date, content, emotion) => {
     dispatch({
       type: "EDIT",
       data: {
         id: targetId,
         date: new Date(date).getTime(),
-        content, emotion,
+        content,
+        emotion,
       },
-    })
-  }
+    });
+  };
 
   return (
     <DiaryStateContext.Provider value={data}>
@@ -114,21 +95,21 @@ function App() {
           onCreate,
           onEdit,
           onRemove,
-          }}
-        >
+        }}
+      >
         <BrowserRouter>
           <div className="App">
             <Routes>
-              <Route path='/' element={<Home/>}/>
-              <Route path='/new' element={<New/>}/>
-              <Route path='/edit' element={<Edit/>}/>
-              <Route path='/diary/:id' element={<Diary/>}/>
+              <Route path="/" element={<Home />} />
+              <Route path="/new" element={<New />} />
+              <Route path="/edit/:id" element={<Edit />} />
+              <Route path="/diary/:id" element={<Diary />} />
             </Routes>
           </div>
         </BrowserRouter>
       </DiaryDispatchContext.Provider>
     </DiaryStateContext.Provider>
-  )
+  );
 }
 
 export default App;
